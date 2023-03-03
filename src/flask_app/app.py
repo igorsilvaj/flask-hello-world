6e6b19e92d12513749387c4e49b6f4e1fac84496
igorsilvaj <igorjsilvabiz@gmail.com>
2023-03-03 11:28:24 -0300

    fix: modify homepage extension to jinja2

--- src/flask_app/app.py
+++ src/flask_app/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return render_template("home.html")
+    return render_template("home.jinja2")
 
 
 @app.route("/teste")
@@ -15,7 +15,7 @@ def teste():
 
 @app.route("/<hello_again>")
 def hello_world_again(hello_again):
-    return render_template("home.html", hello_again=hello_again)
+    return render_template("home.jinja2", hello_again=hello_again)
 
 
 # if __name__ == "__main__":
