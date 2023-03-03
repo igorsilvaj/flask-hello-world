ad3ce34b979d2a650a16adf802b607f3020ab37f
igorsilvaj <igorjsilvabiz@gmail.com>
2023-03-03 10:53:24 -0300

    feat: start repository

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn --pythonpath app src.flask_app.app:app
---
+++ src/flask_app/app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+    return render_template("home.html")
+
+
+@app.route("/teste")
+def teste():
+    return 'teste'
+
+
+@app.route("/<hello_again>")
+def hello_world_again(hello_again):
+    return render_template("home.html", hello_again=hello_again)
+
+
+# if __name__ == "__main__":
+#     app.run(debug=True)
